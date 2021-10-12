8c27d48e5e76f77ecfd7c9b9f315865df3646194
Mahedi Hasan Bijoy <64260243+Mahedi2150@users.noreply.github.com>
2021-10-13 02:57:29 +0600

    local storage

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,10 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./Product.css";
-import Rating from "react-rating";
+
 const Product = (props) => {
     // console.log(props);
-    const { name, img, price, seller, stock,star } = props.product;
+    const { name, img, price, seller, stock } = props.product;
     const cardIcon = <FontAwesomeIcon icon={faShoppingCart} />;
     return (
         <div className="product">
@@ -19,12 +19,7 @@ const Product = (props) => {
                 </p>
                 <h4>Price : ${price}</h4>
                 <p>only {stock} left in stock - order soon</p>
-                <Rating
-                      initialRating={star}
-                    emptySymbol="far fa-star icon-color"
-                    fullSymbol="fas fa-star icon-color"
-                    readonly
-                ></Rating>
+              
                 <br />
                 <button
                     onClick={() => props.hendleAddToCart(props.product)}
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,17 +6,17 @@ import Product from "../Product/Product";
 import "./Shop.css";
 const Shop = () => {
   const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
-    const [displayProduct, setDisplayProduct] = useState([])
+  const [cart, setCart] = useState([]);
+  const [displayProduct, setDisplayProduct] = useState([])
   useEffect(() => {
     fetch("./products.JSON")
       .then((res) => res.json())
-        .then((data) => {
-            setProducts(data);
-            setDisplayProduct(data);
-        });
-  
-      
+      .then((data) => {
+        setProducts(data);
+        setDisplayProduct(data);
+      });
+
+
   }, []);
 
   useEffect(() => {
@@ -39,19 +39,19 @@ const Shop = () => {
     setCart(newCart);
     // save to localStorage
     addToDb(product.key);
-    };
-    const hendleSearch = event => {
-        const searchText = event.target.value;
-        const matchProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
-        setDisplayProduct(matchProducts)
-        console.log(matchProducts.length);
-    }
+  };
+  const hendleSearch = event => {
+    const searchText = event.target.value;
+    const matchProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
+    setDisplayProduct(matchProducts)
+    console.log(matchProducts.length);
+  }
   return (
     <>
       <div className="search-container">
-              <input type="text"
-              onChange={ hendleSearch}
-                placeholder="Search Product" />
+        <input type="text"
+          onChange={hendleSearch}
+          placeholder="Search Product" />
       </div>
       <div className="shop-container">
         <div className="product-container">
